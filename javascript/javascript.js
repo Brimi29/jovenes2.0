2d168d2779ecb90ca82dc7d93731c2726d0f1b4c
brimiangel29@gmail.com <@Brimi29>
2025-06-26 14:47:45 -0500

    actualizo nuevamente

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,4 @@
-let carrusel = document.querySelector('.carrusel');
-let slides = document.querySelectorAll('.carrusel .slide');
 
-let index = 0;
-setInterval(() => {
-  index = (index + 1) % slides.length;
-  carrusel.style.transform = `translateY(-${index * 500}px)`;
-}, 5000);
 console.log("¡Funciona el JavaScript!");
 
 // Aplica animación cuando las cards están en vista
--- javascript/salud.js
+++ javascript/salud.js
@@ -4,7 +4,6 @@ window.addEventListener("load", () => {
 
   setTimeout(() => {
     animacion.style.display = "none";
-    contenido.style.display = "block";
     document.body.style.overflow = "auto";
   }, 7000);
 });
