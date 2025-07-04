2c7c8dd60390cd0a64747de98ff96d362c5f6e4a
Brimi29 <brimiangel29@gmail.com>
2025-07-03 21:18:32 -0500

    actualizo ¡Ya casiii!

--- javascript/problemas.js
+++ javascript/problemas.js
@@ -18,3 +18,22 @@ function toggleCard(cardElement) {
   // Activa/desactiva la card seleccionada
   cardElement.classList.toggle('active');
 }
+// Aquí va el artículo sobre la menstruación
+document.addEventListener("DOMContentLoaded", () => {
+  const bloques = document.querySelectorAll(".arvista-bloque");
+
+  const observar = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+      if (entrada.isIntersecting) {
+        entrada.target.classList.add("visible");
+        observar.unobserve(entrada.target); // para que no se repita
+      }
+    });
+  }, {
+    threshold: 0.1,
+  });
+
+  bloques.forEach(bloque => {
+    observar.observe(bloque);
+  });
+});
